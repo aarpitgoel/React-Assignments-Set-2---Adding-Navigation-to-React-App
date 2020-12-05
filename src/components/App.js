3ecead279922f08a72c10266e2089f8263e736d0
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-12-05 18:28:09 +0530

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
+import { Link, Route, Switch } from "react-router-dom";
+import LocationDisplayComponent from "./LocationDisplayComponent";
 
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+function About() {
+  return <div>You are on the about page.</div>;
+}
+function Home() {
+  return <div>You are home.</div>;
 }
 
+function Invalid() {
+  return <div>No match</div>;
+}
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div id="main">
+          {/* Do not remove the main div */}
+         
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/" exact component={Home} />
+            <Route path="/" component={Invalid} />
+          </Switch>
+          <LocationDisplayComponent />
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
+export const LocationDisplay = LocationDisplayComponent;
